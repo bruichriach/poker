934606f804f48ee0bc153e0a6c6d6bcfc0a24f92
Mark Forshaw <forshaw@atm.ox.ac.uk>
2017-07-18 15:10:54 +0100

    Valid hand testing

--- Poker/Poker/Poker.py
+++ Poker/Poker/Poker.py
@@ -54,6 +54,33 @@ for a in ranks:
 	for b in suits:
 		print(Card(a+b))
 
+def camel_case(message):
+    new_message=""
+    for a in range(0,len(message),2):
+        new_message += message[a].upper()
+        new_message += message[a+1].lower()
+    return new_message
+        
+def valid_cards(hand):
+    if (2*(len(hand)/2)==len(hand)):
+        for a in range(0,len(hand),2):
+            for b in range(0,a,2):
+                if (hand[a:a+2] == hand[b:b+2]):
+                    return False
+            for b in range(0,4):
+                if (hand[a+1] == suits[b]):
+                    break
+                if (b == 3):
+                    return False
+            for b in range(0,13):
+                if (hand[a] == ranks[b]):
+                    break
+                if (b == 12):
+                    return False
+        return True
+    else:
+        return False
+
 game.load_game()
 player = Player()
 game_state=game.state_exists(player.location)
@@ -65,10 +92,13 @@ while game.run:
 			break
 		if action.reqlen != None:
 			if len(action_input) == 2*action.reqlen:
-				player.do_action(action, **action.kwargs)
+				action_input=camel_case(action_input)
+				if valid_cards(action_input):
+					player.do_action(action, **action.kwargs)
 				break
 	game_state=game.state_exists(player.location)
 	game_state.data_output()
 	if player.location == 4:
 		player.location = 0
 		game_state=game.state_exists(player.location)
+
